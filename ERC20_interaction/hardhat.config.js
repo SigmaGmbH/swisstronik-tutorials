6104b5f7785c41fe993036c958ff36b766152251
Santiago Trujillo Zuluaga <stztrujillo@hotmail.com>
2023-08-30 13:22:13 -0500

    Update hardhat.config.js

--- ERC20_interaction/hardhat.config.js
+++ ERC20_interaction/hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-const PRIVATE_KEY = "d5a63c3b252ef2ac23a65320e6e79e5ddd36dd477b9ee20e86e8c0b7d74e80f4";
+const PRIVATE_KEY = "";
 // Remember to use the private key of a testing account
 // For better security practices, it's recommended to use npm i dotenv for storing secret variables
 
