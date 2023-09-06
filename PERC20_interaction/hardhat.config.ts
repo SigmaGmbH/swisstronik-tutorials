45acea79aff7f9f03c386a5b311e5897d1956586
MikkySnow <mike@swisstronik.com>
2023-09-06 12:26:21 +0000

    feat: add deployment script

--- PERC20_interaction/hardhat.config.ts
+++ PERC20_interaction/hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const PRIVATE_KEY = "";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    swisstronik: {
+      // If you're using local testnet, replace `url` with local json-rpc address 
+      url: "https://json-rpc.testnet.swisstronik.com/",
+      accounts: [`0x` + `${PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;
