425a94302da1194dbd4b3ce24def337f5043d12e
your_username <stztrujillo@hotmail.com>
2023-09-07 09:33:40 -0500

    updated compiler version 0.8.19

--- Contract_deployment _hardhat/scripts/deploy.js
+++ Contract_deployment _hardhat/scripts/deploy.js
@@ -1,10 +1,6 @@
 const hre = require("hardhat");
 
 async function main() {
-  /**
-   * @dev make sure the first argument has the same name of your contract in the Hello_swtr.sol file
-   * @dev the second argument is the message we want to set in the contract during the deployment process
-   */
   const contract = await hre.ethers.deployContract("Swisstronik", ["Hello Swisstronik!!"]);
 
   await contract.waitForDeployment();
@@ -12,6 +8,7 @@ async function main() {
   console.log(`Swisstronik contract deployed to ${contract.target}`);
 }
 
+//DEFAULT BY HARDHAT:
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
