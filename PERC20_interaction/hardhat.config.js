4680f97741c4a4432009bd57b157f89acb250404
your_username <stztrujillo@hotmail.com>
2023-09-08 07:05:14 -0500

    changed hardhat file

--- PERC20_interaction/hardhat.config.js
+++ PERC20_interaction/hardhat.config.js
@@ -1,13 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config()
+require("dotenv").config();
 
 module.exports = {
   defaultNetwork: "swisstronik",
   solidity: "0.8.18",
   networks: {
     swisstronik: {
-      // If you're using local testnet, replace `url` with local json-rpc address 
-      url: "http://localhost:8545",
+      // If you're using local testnet, replace `url` with local json-rpc address
+      url: "https://json-rpc.testnet.swisstronik.com/",
       accounts: [`0x` + `${process.env.PRIVATE_KEY}`],
     },
   },
