1afd963619ecf3955e3f0f5a99f2e8807dec0a53
your_username <stztrujillo@hotmail.com>
2023-09-27 19:24:16 +0100

    changed waitForDeployment() for deployed()

--- PERC20_interaction/scripts/deploy.js
+++ PERC20_interaction/scripts/deploy.js
@@ -2,9 +2,9 @@ const { ethers } = require("hardhat");
 
 async function main() {
   const perc20 = await ethers.deployContract("PERC20Sample");
-  await perc20.waitForDeployment();
-  
-  console.log(`PERC20Sample was deployed to ${perc20.address}`)
+  await perc20.deployed();
+
+  console.log(`PERC20Sample was deployed to ${perc20.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
