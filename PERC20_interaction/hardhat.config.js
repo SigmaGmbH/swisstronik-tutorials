0d4dc242b1a2737e932b011532fb514ceba959bd
your_username <stztrujillo@hotmail.com>
2023-09-29 13:42:29 +0100

    resolved conflict in hardhat.config.js

--- PERC20_interaction/hardhat.config.js
+++ PERC20_interaction/hardhat.config.js
@@ -6,11 +6,7 @@ module.exports = {
   solidity: "0.8.18",
   networks: {
     swisstronik: {
-<<<<<<< HEAD
       // If you're using local testnet, replace `url` with local json-rpc address
-=======
-      // If you're using local testnet, replace `url` with local json-rpc address 
->>>>>>> 16f446d9da9d7c449eb73ae6d44d3930df14f4a0
       url: "https://json-rpc.testnet.swisstronik.com/",
       accounts: [`0x` + `${process.env.PRIVATE_KEY}`],
     },
