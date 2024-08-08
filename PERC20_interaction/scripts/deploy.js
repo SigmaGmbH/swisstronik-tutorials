3c73ee861e316181d1b099fa70ebf55c79608d6b
Intellygentle <57158297+intellygentle@users.noreply.github.com>
2024-08-08 14:26:28 +0100

    Update deploy.js (#26)

--- PERC20_interaction/scripts/deploy.js
+++ PERC20_interaction/scripts/deploy.js
@@ -2,9 +2,10 @@ const { ethers } = require("hardhat");
 
 async function main() {
   const perc20 = await ethers.deployContract("PERC20Sample");
-  await perc20.deployed();
+    // Wait for the transaction to be mined
+  await perc20.waitForDeployment();
 
-  console.log(`PERC20Sample was deployed to ${perc20.address}`);
+  console.log(`PERC20Sample was deployed to ${await perc20.getAddress()}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -13,3 +14,7 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+// The deployed() function is no longer available on the contract instance returned by deployContract(). 
+// Instead, the contract is considered deployed as soon as deployContract() resolves.
+// this modified script solved the error of "TypeError: perc20.deployed is not a function"
