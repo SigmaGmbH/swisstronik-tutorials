f19a05941ba9909a067f4af89833554be66a950d
Lesther Caballero <itslesther@Lesthers-MacBook-Pro.local>
2024-09-06 16:21:04 +0200

    example with plugin

--- ERC20_interaction/scripts/mint.js
+++ ERC20_interaction/scripts/mint.js
@@ -1,47 +1,25 @@
 // Import necessary modules from Hardhat and SwisstronikJS
 const hre = require("hardhat");
-const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
+const { SwisstronikPlugin } = require("@swisstronik/web3-plugin-swisstronik");
 
-// Function to send a shielded transaction using the provided signer, destination, data, and value
-const sendShieldedTransaction = async (signer, destination, data, value) => {
-  // Get the RPC link from the network configuration
-  const rpcLink = hre.network.config.url;
-
-  // Encrypt transaction data
-  const [encryptedData] = await encryptDataField(rpcLink, data);
-
-  // Construct and sign transaction with encrypted data
-  return await signer.sendTransaction({
-    from: signer.address,
-    to: destination,
-    data: encryptedData,
-    value,
-  });
-};
+hre.web3.registerPlugin(new SwisstronikPlugin(hre.network.config.url));
 
 async function main() {
   // Address of the deployed contract
   const contractAddress = "0x7D804090e7a1FF0709d743d115bccE6757Bbe208";
 
   // Get the signer (your account)
-  const [signer] = await hre.ethers.getSigners();
+  const [from] = await hre.web3.eth.getAccounts();
 
   // Create a contract instance
   const contractFactory = await hre.ethers.getContractFactory("TestToken");
-  const contract = contractFactory.attach(contractAddress);
 
-  // Send a shielded transaction to mint 100 tokens in the contract
-  const functionName = "mint100tokens";
-  const mint100TokensTx = await sendShieldedTransaction(
-    signer,
-    contractAddress,
-    contract.interface.encodeFunctionData(functionName),
-    0
-  );
+  const ABI = JSON.parse(contractFactory.interface.formatJson());
 
-  await mint100TokensTx.wait();
+  const contract = new hre.web3.eth.Contract(ABI, contractAddress);
 
-  // It should return a TransactionReceipt object
+  const mint100TokensTx = await contract.methods.mint100tokens().send({ from });
+
   console.log("Transaction Receipt: ", mint100TokensTx);
 }
 
