26b5299305e4454c58036176de6c83ad6f3d5721
Lesther Caballero <lesther.tech@gmail.com>
2024-09-09 11:20:24 +0200

    Ethers completely removed from mint example (#30)

--- ERC20_interaction/scripts/mint.js
+++ ERC20_interaction/scripts/mint.js
@@ -1,23 +1,22 @@
 // Import necessary modules from Hardhat and SwisstronikJS
-const hre = require("hardhat");
+const { network, web3 } = require("hardhat");
+const { abi } = require("../artifacts/contracts/Token.sol/TestToken.json");
 const { SwisstronikPlugin } = require("@swisstronik/web3-plugin-swisstronik");
 
-hre.web3.registerPlugin(new SwisstronikPlugin(hre.network.config.url));
-
 async function main() {
+  // Register the Swisstronik plugin
+  web3.registerPlugin(new SwisstronikPlugin(network.config.url));
+
   // Address of the deployed contract
   const contractAddress = "0x7D804090e7a1FF0709d743d115bccE6757Bbe208";
 
   // Get the signer (your account)
-  const [from] = await hre.web3.eth.getAccounts();
+  const [from] = await web3.eth.getAccounts();
 
   // Create a contract instance
-  const contractFactory = await hre.ethers.getContractFactory("TestToken");
+  const contract = new web3.eth.Contract(abi, contractAddress);
 
-  const ABI = JSON.parse(contractFactory.interface.formatJson());
-
-  const contract = new hre.web3.eth.Contract(ABI, contractAddress);
-
+  // Call the mint100tokens function
   const mint100TokensTx = await contract.methods.mint100tokens().send({ from });
 
   console.log("Transaction Receipt: ", mint100TokensTx);
