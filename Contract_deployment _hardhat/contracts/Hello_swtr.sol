bb0c0e8d7672b217efccf598bf88b93bc9a89338
Denis <dev-adv@swisstronik.com>
2025-02-24 17:05:23 +0400

    Added example for second challenge

--- Contract_deployment _hardhat/contracts/Hello_swtr.sol
+++ Contract_deployment _hardhat/contracts/Hello_swtr.sol
@@ -1,17 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.24;
+import {ISWTRProxy} from "@swisstronik/sdi-contracts/contracts/interfaces/ISWTRProxy.sol";
 
 //This contract is only intended for testing purposes
 
 contract Swisstronik {
     string private message;
 
+
+    ISWTRProxy public swtrProxy;
+
+    function getLatestIssuers() public returns (address[]) {
+        uint256 count = swtrProxy.issuerRecordCount();
+
+        //TODO: rest of the function
+    }
+
     /**
      * @dev Constructor is used to set the initial message for the contract
      * @param _message: The message to be associated to the message variable
      */
-    constructor(string memory _message) payable{
+    constructor(string memory _message) payable {
         message = _message;
+        swtrProxy = ISWTRProxy(0xBF896E5616d12fE6Bd7a376D2DBb924ff531CFDF);
     }
 
     /**
